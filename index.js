b955f20d8edd8ff24805f509527233f767a03601
Zeexon <sorokinn6@gmail.com>
2024-03-16 18:35:47 +1000

    CNY calculator

--- index.js
+++ index.js
@@ -2,24 +2,50 @@
 const TelegramBot = require('node-telegram-bot-api');
 const config = require('config');
 const express = require('express');
+const fs = require('fs')
 const app = express();
 const webAppUrl = 'https://total-geek.ru'
+const rublePhotoPath = './imgs/ruble_image.jpg'
+const rublePhoto = fs.readFileSync(rublePhotoPath)
+const CNYPhotoPath = './imgs/CNYimg.jpg'
+const CNYPhoto = fs.readFileSync(CNYPhotoPath)
 
 const token = config.get('TELEGRAM_TOKEN');
 let chatId;
 let calcFlag = false;
-let findItemFlag = false;
-let orderButtonData = {title:'',price:''}
 
 const bot = new TelegramBot(token, { polling: true });
+
+function CNYCalculation(message) {
+    let messageInCNY = message / 12.75
+    let additionalAmount = 0;
+
+    if (messageInCNY > 200 && messageInCNY < 500) {
+        additionalAmount = 3000 / 12.75;
+    } else if (messageInCNY >= 500 && messageInCNY < 1000) {
+        additionalAmount = 4000 / 12.75;
+    }
+
+    return (parseFloat(messageInCNY + additionalAmount)) ;
+}
+
 
 bot.on('message', async (msg) => {
     chatId = msg.chat.id;
     const text = msg.text;
 
+    if (calcFlag) {
+        const message = msg.text;
+        const messageNumber = CNYCalculation(message)
+        await bot.sendPhoto(chatId, CNYPhoto)
+        await bot.sendMessage(chatId, messageNumber.toFixed(2) + ' CNY');
+        calcFlag = false;
+    }
+
     switch (text) {
-        case 'Рассчитать RUB --> CNY':
+        case 'ЗАКАЗАТЬ С POIZON':
             await bot.sendMessage(msg.from.id, 'Введите сумму в RUB \u2192');
+            await bot.sendPhoto(msg.from.id, rublePhoto)
             await bot.sendMessage(msg.from.id, 'Формула здесь-->');
             calcFlag = true;
             break;
@@ -28,28 +54,18 @@ bot.on('message', async (msg) => {
     }
 
     if (text === '/start') {
-        await bot.sendMessage(chatId, 'Привет, это Rain Zone Bot!\nПомогу подобрать одежду и рассчитать стоимость из RUB в CNY, выбери, чем я могу тебе помочь!\nPS: Если тебя интересует наш полный каталог, то жми большую кнопку "ОФОРМИТЬ ЗАКАЗ" внизу экрана', {
+        await bot.sendMessage(chatId, 'Привет, это Rain Zone Bot!\nПомогу подобрать одежду и рассчитать стоимость из RUB в CNY, выбери, чем я могу тебе помочь!\nPS: Если тебя интересует наш каталог товаров которые уже у нас в наличии, то жми большую кнопку "ОФОРМИТЬ ЗАКАЗ" внизу экрана \n Елси ты хочешь заказать прямо с  POIZON жми кнопку "ЗАКАЗАТЬ С POIZON"', {
             reply_markup: {
                 inline_keyboard: [
 
                 ],
                 keyboard : [
                     [{text: '✅ ОФОРМИТЬ ЗАКАЗ 👟', web_app: {url: webAppUrl}}],
-                    [{ text: 'Рассчитать RUB --> CNY'}],
+                    [{ text: 'ЗАКАЗАТЬ С POIZON'}],
                 ]
             },
         });
 
-    }
-    if(findItemFlag){
-        await bot.sendMessage(msg.from.id, 'Sorry this feature is still in process');
-        findItemFlag = false;
-    }
-    if (calcFlag) {
-        const message = msg.text;
-        const messageNumber = parseFloat(message) / 12.75;
-        await bot.sendMessage(chatId, messageNumber.toFixed(2) + ' CNY');
-        calcFlag = false;
     }
     if(msg?.web_app_data?.data){
         try{
@@ -64,46 +80,5 @@ bot.on('message', async (msg) => {
     }
 });
 
-app.get('/getChatId', (req,res) => {
-    res.json({chatId})
-})
 
-app.post('/inmess', (req, res) => {
-    const { title, price } = req.body;
-    orderButtonData = {title:title, price:price}
 
-    bot.sendMessage(chatId, `Received data:\nTitle: ${title}\nPrice: ${price}`)
-    .then(() => {
-      console.log('Message sent successfully');
-      res.json({ success: true });
-    })
-    .catch((error) => {
-      console.error('Error sending message:', error);
-      res.status(500).json({ success: false, error: 'Failed to send message' });
-    });
-
-  
-    console.log('Received data from the website:', { title, price });
-  
-    res.json({ success: true });
-});
-
-// bot.on('callback_query', async (msg) => {
-//     const buttonData = msg.data; 
-
-    // switch (buttonData) {
-    //     case 'calc':
-    //         await bot.sendMessage(msg.from.id, 'Введите сумму в RUB \u2192');
-    //         await bot.sendMessage(msg.from.id, 'Формула здесь-->');
-    //         calcFlag = true;
-    //         break;
-    //     case 'find_item':
-    //         await bot.sendMessage(msg.from.id, 'Введите конкретное наименование товара или его стоимость');
-    //         findItemFlag = true;
-    //         break;
-    //     default:
-    //         break;
-    // }
-
-//     await bot.answerCallbackQuery(msg.id);
-// });
